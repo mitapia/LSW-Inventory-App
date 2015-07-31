798659b27413f5427bbad86251f7bb6a4b549429
Miguel Tapia <migueltapia.it@gmail.com>
2015-07-31 14:44:05 -0500

    added view addition

---
+++ inventory_app/resources/views/include/table_heads.blade.php
@@ -0,0 +1,8 @@
+	<!-- Currently running on online resources must change to local source file at time of production -->
+	<!-- In order to use Handsontable as a jQuery plugin -->
+	<!-- <script src="handsontable/lib/jquery.min.js"></script> -->
+	<script src="//code.jquery.com/jquery-1.11.3.min.js"></script> <!-- online -->
+
+	<!-- this pulls files needed files for Handontable -->
+	<script src="http://handsontable.com/dist/handsontable.full.js"></script>
+	<link rel="stylesheet" media="screen" href="http://handsontable.com/dist/handsontable.full.css">
